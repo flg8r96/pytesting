0ed1db1b03f25778f01420b2c87da5ab2c1138c2
flg8r96 <flg8r96@gmail.com>
2014-08-07 15:52:10 -0700

    cleaned up the code and set the normalization to happen automatically

--- rgb.py
+++ rgb.py
@@ -14,6 +14,11 @@ new_file = "/Users/flg8r96/love_gray.jpg"
 lined_file = "/Users/flg8r96/love_gray_lines.jpg"
 pxl_file = "/Users/flg8r96/love_gray_pixel.jpg"
 
+file = "/Users/flg8r96/a.jpg"
+new_file = "/Users/flg8r96/a_gray.jpg"
+lined_file = "/Users/flg8r96/a_gray_lines.jpg"
+pxl_file = "/Users/flg8r96/a_gray_pixel.jpg"
+
 #file = "/Users/flg8r96/bar.jpg"
 #new_file = "/Users/flg8r96/bar_gray.jpg"
 #lined_file = "/Users/flg8r96/bar_gray_lines.jpg"
@@ -44,20 +49,21 @@ gwidth, gheight = grayscale.size
 print "Grayscale size: " + str(gwidth) +"x" +str(gheight)
 
 
-row_deviations = 20
-col_deviations = 20
+row_deviations = 16
+col_deviations = 16
 
 col_cnt = 0
 row_cnt = 0
 
 # create an array of summed pixel values
-# initialize the array
-#light = [0 for x in xrange(0, col_deviations)] # col in array
-#sumlight = [light for x in xrange(0, row_deviations)] # row in array
+# initialize the array - HOW THIS ARRAY IS CREATED IS IMPORTANT
+# IF YOU DON'T CREATE THE ARRAY AS SHOWN BELOW, THE ELEMENTS CAN'T BE ACCESSED BY ROW/COL
 sumlight = [[0]*col_deviations for x in range(row_deviations)] # row in array
 
-
-
+##### Extract the pixels from the photo anc create an interpolated version
+# row, col represent the row, col for each pixel in the original image
+# row_cnt, col_cnt represent the pixels in the downsampled (pixelated) "image" - this image will be
+# represented by the array of LEDs
 # search accross and then down for the grid line indexes
 for row in xrange(0, gheight):
     col_cnt = 0;
@@ -71,36 +77,34 @@ for row in xrange(0, gheight):
             row_cnt += 1
 
         #print "pixel for col/row: "  +str(col) +"/" +str(row) +"-" +str(pixelrssi) +" col_cnt/row_cnt:"  +str(col_cnt) +"/" +str(row_cnt)
+        """
+        # THIS CAN BE USED TO CREATE A BLACK AND WHITE PIXEL GRID
         if pixelrssi > 250:
             # add nothing if white
             a = float(0)
-
         else:
             # add 1 if black
             a = float(1)
-            # row and col are flipped when referencing the array
-        sumlight[row_cnt][col_cnt] += 255 - float(pixelrssi)
             #sumlight[row_cnt][col_cnt] += a
+            """
 
-        #sumlight[col_cnt][row_cnt] += pixelrssi/float(255)
-        #sumlight[col_cnt][row_cnt] += a
+        # row and col are flipped when referencing the array
+        sumlight[row_cnt][col_cnt] += 255 - float(pixelrssi)
 
         # draw horizontal line on the image to see where the larger pixels are located
         if row%(gheight/row_deviations) == 0:
-            #print "row/col: " +str(row) +"/" +str(col)
             grayscale.putpixel((col, row), 0) # black horizontal line
-            # increment row_cnt but don't do it after the first real pixel row
+            # increment row_cnt but don't do it until after the first real pixel row
             if col == 0 and row != 0:
                 row_cnt += 1
 
         if col%(gwidth/col_deviations) == 0:
             #print "Changing a col pixel"
             grayscale.putpixel((col, row), 0) # black vertical line
-            # increment col_cnt
             if col != 0:
                 col_cnt += 1
 
-#msg = grayscale.save("/Users/flg8r96/love_gray_lines.jpg")
+#
 msg = grayscale.save(lined_file)
 print "Error in writing file: " +str(msg)
 print "row_cnt/col_cnt:" +str(row_cnt) +"/" +str(col_cnt)
@@ -110,18 +114,16 @@ print sumlight
 row_pixels = gheight/float(row_deviations)
 col_pixels = gwidth/float(col_deviations)
 pixels_per_square = row_pixels * col_pixels
-#norm_sumlight = [ x/10 for x in sumlight]
 norm_sumlight = numpy.divide(sumlight,pixels_per_square)
-#print norm_sumlight
-
 norm_sumlight = numpy.around(norm_sumlight, decimals=0)
 print norm_sumlight
-print norm_sumlight[9][9]
-print norm_sumlight[6][6]
-print type(norm_sumlight[6][6])
 
-raw_input("enter")
+rssimax = norm_sumlight.max()
+# pause the processing to visually inspect the result of the pixelated picture
+#raw_input("enter")
 
+# reconstruct a pixelated image for final inspection (this is what it will look like
+# ... when the led array is used)
 col_cnt=0
 row_cnt=0
 print "Starting to create the pixelated file"
@@ -132,27 +134,9 @@ for row in xrange(0, gheight):
     for col in xrange(0, gwidth):
 
         # get current color value and add it to the sumlight for the grid that this pixel is in
-#try:
-    #if norm_sumlight[row_cnt][col_cnt] != 0:
-        print col_cnt, row_cnt, norm_sumlight[row_cnt][col_cnt]
-        val1 = (norm_sumlight[row_cnt][col_cnt])
-
-        val2 = val1*255/180
-        val = int(val2)
-        print val1, val2, val
-        #if val1 > 103:
-            #raw_input("ENTER")
-        #print val
+        #print col_cnt, row_cnt, norm_sumlight[row_cnt][col_cnt]
+        val = int((norm_sumlight[row_cnt][col_cnt])*255/rssimax)
         pixelated.putpixel((col, row), val)   # black pixel
-        print "pixel for col/row: "  +str(col) +"/" +str(row) +"-" +str(val1) +str("..") +str(val)  +" col_cnt/row_cnt:"  +str(col_cnt) +"/" +str(row_cnt)
-    #else:
-    #    pixelated.putpixel((col, row), 255) # white pixel
-#except Exception, err:
-    #print traceback.format_exc()
-
-        # not sure this does a damn thing
-        #if col_cnt == 0 and row%(gheight/row_deviations) != 0 and col == gwidth-1:
-        #    row_cnt += 1
 
         # draw horizontal line on the image to see where the larger pixels are located
         if row%(gheight/row_deviations) == 0:
@@ -162,10 +146,10 @@ for row in xrange(0, gheight):
         if col%(gwidth/col_deviations) == 0:
             if col != 0:
                 col_cnt += 1
-                #raw_input("enter")
 
 try:
     msg2 = pixelated.save(pxl_file)
+    print "Pixelated file saved!"
 except Exception, err:
     print traceback.format_exc()
     print "Save of pixelated file didn't work"
